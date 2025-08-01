16152a6b8aaa19842b39ebf1b86f9642477ea835
huy nguyen <huytdhd@gmail.com>
2025-08-02 00:10:32 +0700

    logic schedule update view comic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,9 @@
 namespace App\Console;
 
 use App\Jobs\FectchComicsDaily;
+use App\Jobs\ResetViewPerDayJob;
+use App\Jobs\ResetViewPerMonthJob;
+use App\Jobs\ResetViewPerWeekJob;
 use App\Jobs\UpdateComicDescriptionJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -17,6 +20,9 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->job(new FectchComicsDaily)->hourly();
         $schedule->job(new UpdateComicDescriptionJob)->dailyAt('09:00');
+        $schedule->job(new ResetViewPerDayJob)->dailyAt('02:00');
+        $schedule->job(new ResetViewPerWeekJob)->mondays();
+        $schedule->job(new ResetViewPerMonthJob)->monthly();
     }
 
     /**
